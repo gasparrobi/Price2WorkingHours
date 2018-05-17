f2c96279a30977cf6cd45984c2774411e89e9104
Robert Gaspar <gasparrobi@gmail.com>
2018-05-17 14:19:00 +0200

    working hours comes from user input

--- main.js
+++ main.js
@@ -1,18 +1,20 @@
 let form = document.querySelector("#ext-form");
 let selector = document.querySelector("#extActive");
 let inp = document.querySelector("#monthlyNetWage");
+let workingHours = document.querySelector('#weeklyWorkingHours')
 
 chrome.storage.local.get(
-  ["monthlyNetWage", "extensionActive"],
+  ["monthlyNetWage", "extensionActive", "workingHours"],
   async result => {
     inp.value = result.monthlyNetWage || "";
     selector.value = result.extensionActive || true;
+    workingHours.value = result.workingHours || 40;
   }
 );
 
 form.addEventListener("submit", event => {
   chrome.storage.local.set(
-    { monthlyNetWage: inp.value, extensionActive: selector.value },
+    { monthlyNetWage: inp.value, extensionActive: selector.value, workingHours: workingHours.value || 40 },
     function() {}
   );
   chrome.tabs.query({ active: true, currentWindow: true }, function(tabs) {
