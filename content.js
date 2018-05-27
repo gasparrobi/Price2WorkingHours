4eb14583ba42468abb6cae3167181717b3997fdb
Robert Gaspar <gasparrobi@gmail.com>
2018-05-27 13:34:03 +0200

    preserving original price fixed everywhere, code cleanup, jsdoc

--- content.js
+++ content.js
@@ -31,11 +31,12 @@ chrome.storage.local.get(
 );
 
 function manipulateArukereso() {
-  const prices = document.querySelectorAll("a.price, .item a.name span");
-  const prices3 = document.querySelectorAll(
-    'span[itemprop="price"], div.price, span.price, div[itemprop="offers"] a span'
-  );
+  let selectors =
+    'span[itemprop="price"], div.price, span.price, div[itemprop="offers"] a span';
 
+  changeNormalPrice(selectors);
+
+  const prices = document.querySelectorAll("a.price, .item a.name span");
   prices.forEach(item => {
     let price = parseInt(
       item.innerText
@@ -45,15 +46,16 @@ function manipulateArukereso() {
         .join("")
     );
     let hour = parseFloat(price / hourly).toFixed(1);
-    item.innerText = `${hour} ${hrString}`;
-  });
-
-  prices3.forEach(item => {
-    changeNormalPrice(item);
+    displayPrice(item, hour);
   });
 }
 
 function manipulateAlza() {
+  // PRODUCT PAGE
+  let selectors =
+    "span.price_withVat, span.price_withoutVat, span.price, .priceInner .c2, .b32";
+  changeNormalPrice(selectors);
+
   // MAIN PAGE
   let priceCenter = document.querySelectorAll(".priceCenter");
   let prices = [];
@@ -63,53 +65,18 @@ function manipulateAlza() {
   });
 
   prices.forEach(item => {
-    changeNormalPrice(item);
-  });
-
-  // PRODUCT PAGE
-  let prices2 = document.querySelectorAll(
-    "span.price_withVat, span.price_withoutVat, span.price"
-  );
-  prices2.forEach(item => {
-    changeNormalPrice(item);
-  });
-
-  // PRODUCT LIST
-  let prices3 = document.querySelectorAll(".priceInner .c2, .b32");
-  prices3.forEach(item => {
-    changeNormalPrice(item);
+    let price = parseInt(
+      origText
+        .split("Ft")
+        .join("")
+        .trim()
+        .split(/\s+/)
+        .join("")
+    );
+    let hour = parseFloat(price / hourly).toFixed(1);
+    displayPrice(item, hour);
   });
 }
-
-// istyle updated their design
-// TODO
-
-// function manipulateIstyleEu() {
-//   // PRODUCT PAGE
-//   const prices = document.querySelectorAll(
-//     'span[itemprop="price"], span.price span'
-//   );
-//   const prices2 = document.querySelectorAll(".price-wrapper .updated");
-
-//   prices.forEach(item => {
-//     changeNormalPrice(item);
-//   });
-
-//   prices2.forEach(item => {
-//     let price = parseInt(
-//       item.innerText
-//         .split("Ft")
-//         .join("")
-//         .split("Price from:")
-//         .join("")
-//         .trim()
-//         .split(/\s+/)
-//         .join("")
-//     );
-//     let hour = parseFloat(price / hourly).toFixed(1);
-//     item.innerText = `${hour} ${hrString}`;
-//   });
-// }
 
 function manipulateEdigitalHu() {
   // PRODUCT PAGE
@@ -118,37 +85,58 @@ function manipulateEdigitalHu() {
   mainPriceItems.forEach(item => {
     let price = parseInt(item.getAttribute("content"));
     let hour = parseFloat(price / hourly).toFixed(1);
-    item.innerText = `${hour} ${hrString}`;
+    displayPrice(item, hour);
   });
 
   // MAIN PAGE AND OTHER LISTINGS
-  let prices = document.querySelectorAll("span.price");
-
-  prices.forEach(item => {
-    changeNormalPrice(item);
-  });
+  let selectors = "span.price";
+  changeNormalPrice(selectors);
 }
 
 // HELPER FUNCTIONS
 
-// function for common price display usage, eg: '10000 Ft'
-function changeNormalPrice(item) {
-  let origText = item.innerText;
-  let price = parseInt(
-    origText
-      .split("Ft")
-      .join("")
-      .trim()
-      .split(/\s+/)
-      .join("")
-  );
-  let hour = parseFloat(price / hourly).toFixed(1);
-  item.innerHTML = `${hour} ${hrString} <small>(${origText})</small>`;
+/**
+ * function for common price display usage, eg: '10000 Ft'
+ *
+ * @param {string} selectors list of selectors divided by comma
+ */
+function changeNormalPrice(selectors) {
+  let items = document.querySelectorAll(selectors);
+
+  items.forEach(item => {
+    let price = parseInt(
+      item.innerText
+        .split("Ft")
+        .join("")
+        .trim()
+        .split(/\s+/)
+        .join("")
+    );
+    let hour = parseFloat(price / hourly).toFixed(1);
+    displayPrice(item, hour);
+  });
 }
 
-// just a setTimeout wrapper
+/**
+ * wrapper for delaying function execution,
+ *
+ * @param {function} fn function to be executed
+ * @param {number} delay delay in milliseconds
+ */
 function delayedRun(fn, delay) {
   setTimeout(() => {
     fn();
   }, delay);
+}
+
+/**
+ * displaying price in working hours while preserving original price
+ *
+ * @param {DOM Element} item
+ * @param {number} hour
+ */
+function displayPrice(item, hour) {
+  item.innerHTML = `${hour} ${hrString} <small style="font-size: 12px">(${
+    item.innerText
+  })</small>`;
 }
